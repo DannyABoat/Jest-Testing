3bea787cad9c12e8a35e5bf56fa8b890976af672
Daniel A. Boateng <92589473+DannyABoat@users.noreply.github.com>
2022-01-19 00:19:39 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// let add = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// let sub = (num1, num2) => {
+//     return num1 - num2;
+// }
+
+// let myArray = ['Al', 'John', 'Jake'];
+
+// module.exports = {
+//     name: "Testing",
+//     desc: "Jest testing",
+//     add,
+//     sub,
+//     myArray,
+// }
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
+
+module.exports = {
+    add,
+    movies: [
+        "pirates of the carib",
+        "terminator",
+        "avatar"
+    ]
+}
